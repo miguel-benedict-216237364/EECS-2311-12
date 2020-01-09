298329e7e48355587030c2626082f04ca5143afe
Wang-Hongru-216403370 <46507650+bruceWang123@users.noreply.github.com>
2020-01-09 00:04:35 -0500

    new change

--- venn/src/main/java/venn/Main.java
+++ venn/src/main/java/venn/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("I have access, Benedict");
 		System.out.println("I have access test 2, Benedict");
 		System.out.println("I have access test 3, Prince");
+		System.out.println("I have access test 4, Bruce");
 	}
 
 }
