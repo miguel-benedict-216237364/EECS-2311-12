12e3399bc331565f8ee5ec2ca0fc8ed322c5b205
Drawde89 <eds89@my,yorku.ca>
2020-01-11 08:11:30 -0500

    Edward has access to Git

--- venn/src/main/java/venn/Main.java
+++ venn/src/main/java/venn/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		System.out.println("I have access test 2, Benedict");
 		System.out.println("I have access test 3, Prince");
 		System.out.println("I have access test 4, Bruce");
+		System.out.println("I have access test 5, Edward");
 	}
 
 }
