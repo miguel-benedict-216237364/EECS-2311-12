f28e775ffe73b8beee4f8956f4535078bd52c471
miguel-benedict-216237364 <ben106@my.yorku.ca>
2020-01-13 11:45:55 -0500

    Test for windows builder

---
+++ venn/src/main/java/venn/vennDiagram.java
@@ -0,0 +1,43 @@
+package venn;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class vennDiagram {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vennDiagram window = new vennDiagram();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public vennDiagram() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
