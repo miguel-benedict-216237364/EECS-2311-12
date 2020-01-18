27885aea40950342c07ede882493bdd21bb2a0dc
babbu16 <babbu@my.yorku.ca>
2020-01-18 15:15:36 -0500

    Panels for Venn Diagram

--- venn/src/main/java/venn/vennDiagram.java
+++ venn/src/main/java/venn/vennDiagram.java
@@ -12,11 +12,12 @@ import javax.swing.JScrollPane;
 import java.awt.GridBagLayout;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class vennDiagram {
 
 	private JFrame frame;
-	private JTextField txtThisWork;
 
 	/**
 	 * Launch the application.
@@ -48,15 +49,48 @@ public class vennDiagram {
 		frame = new JFrame();
 		frame.getContentPane().setLayout(null);
 		
-		txtThisWork = new JTextField();
-		txtThisWork.setText("This works!");
-		txtThisWork.setBounds(27, 44, 130, 26);
-		frame.getContentPane().add(txtThisWork);
-		txtThisWork.setColumns(10);
+		JButton btnAddLeft = new JButton("+Add");
+		btnAddLeft.setForeground(Color.GREEN);
+		btnAddLeft.setBackground(Color.BLACK);
+		btnAddLeft.setBounds(23, 209, 98, 26);
+		frame.getContentPane().add(btnAddLeft);
 		
-		JTextPane txtpnThisWorks = new JTextPane();
-		txtpnThisWorks.setText("This works!");
-		txtpnThisWorks.setBounds(80, 124, 178, 16);
-		frame.getContentPane().add(txtpnThisWorks);
+		JButton btnAddCenter = new JButton("+Add");
+		btnAddCenter.setForeground(Color.GREEN);
+		btnAddCenter.setBackground(Color.BLACK);
+		btnAddCenter.setBounds(170, 209, 98, 26);
+		frame.getContentPane().add(btnAddCenter);
+		
+		JButton btnAddRight = new JButton("+Add");
+		btnAddRight.setForeground(Color.GREEN);
+		btnAddRight.setBackground(Color.BLACK);
+		btnAddRight.setBounds(308, 209, 98, 26);
+		frame.getContentPane().add(btnAddRight);
+		
+		JTextPane txtpnLeft = new JTextPane();
+		txtpnLeft.setToolTipText("Left\r\n");
+		txtpnLeft.setSelectedTextColor(Color.MAGENTA);
+		txtpnLeft.setBackground(Color.BLACK);
+		txtpnLeft.setFont(new Font("Dialog", Font.BOLD, 14));
+		txtpnLeft.setForeground(Color.GREEN);
+		txtpnLeft.setText("           Left");
+		txtpnLeft.setBounds(12, 12, 124, 237);
+		frame.getContentPane().add(txtpnLeft);
+		
+		JTextPane txtpnCenter = new JTextPane();
+		txtpnCenter.setBackground(Color.BLACK);
+		txtpnCenter.setFont(new Font("Dialog", Font.BOLD, 14));
+		txtpnCenter.setForeground(Color.GREEN);
+		txtpnCenter.setText("        Center");
+		txtpnCenter.setBounds(155, 12, 124, 237);
+		frame.getContentPane().add(txtpnCenter);
+		
+		JTextPane txtpnRight = new JTextPane();
+		txtpnRight.setFont(new Font("Dialog", Font.BOLD, 14));
+		txtpnRight.setForeground(new Color(0, 255, 0));
+		txtpnRight.setBackground(Color.BLACK);
+		txtpnRight.setText("          Right");
+		txtpnRight.setBounds(298, 12, 124, 237);
+		frame.getContentPane().add(txtpnRight);
 	}
 }
