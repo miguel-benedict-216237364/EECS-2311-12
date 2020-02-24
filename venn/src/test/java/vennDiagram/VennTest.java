ee289def992d6f1d08158a0ee0fb1e76c36b6284
Drawde89 <eds89@my.yorku.ca>
2020-02-23 23:03:20 -0500

    Added Test Cases

--- venn/src/test/java/vennDiagram/VennTest.java
+++ venn/src/test/java/vennDiagram/VennTest.java
@@ -1,18 +1,67 @@
 package vennDiagram;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import org.junit.Test;
 
 public class VennTest {
 	private VennDiagram venn;
+
 	@Test
+
 	public void test() {
 		venn = new VennDiagram();
-		venn.initialize();
-		venn.initializeTwo();
-		venn.initializeMenu();
-		
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+
 	}
 
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.main(null);
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test6() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
 }
