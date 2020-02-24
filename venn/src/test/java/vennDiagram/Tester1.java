979a37f81708670adfc11b99693988ec709e22f1
babbu16 <babbu@my.yorku.ca>
2020-02-24 11:49:02 -0500

    Testing

--- venn/src/test/java/vennDiagram/Tester1.java
+++ venn/src/test/java/vennDiagram/Tester1.java
@@ -487,5 +487,293 @@ public class Tester1 {
 		VennDiagram.initializeTwo();
 		VennDiagram.initializeMenu();
 		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
+	}
+	@Test
+	public void test2() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.refresh();
+	}
+
+	@Test
+	public void test3() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.resortTextAreaList(new ArrayList<JPanel>());
+	}
+	
+	
+	@Test
+	public void test4() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.lowestTextArea(new ArrayList<JPanel>());
+	}
+	
+	@Test
+	public void test5() {
+		venn = new VennDiagram();
+		VennDiagram.initialize();
+		VennDiagram.initializeTwo();
+		VennDiagram.initializeMenu();
+		VennDiagram.getWrappedLines(new JTextArea("Hello"));
 	}*/
 }
