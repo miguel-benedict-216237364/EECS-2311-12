13d34eec4937ec9e36a59fcbdb8aea4d03b7765c
babbu16 <babbu@my.yorku.ca>
2020-02-24 11:57:17 -0500

    Acceptance test case

--- venn/src/main/java/edsVenn/Main.java
+++ venn/src/main/java/edsVenn/Main.java
@@ -23,4 +23,45 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	/* Acceptance Test Cases
+	 * •	If the user clicks the middle 
+	 * of the Venn diagram and types, 
+	 * after deleting ‘This is text number 1’:
+
+“His palms are sweaty, knees weak, arms are heavy. 
+There's vomit on his sweater already, 
+mom's spaghetti. He's nervous, 
+but on the surface, 
+he looks calm and ready. 
+To drop bombs, but he keeps on forgettin. 
+What he wrote down, 
+the whole crowd goes so loud. He opens his mouth, 
+but the words won't come out. 
+He's chokin' how, 
+everybody's jokin' now. 
+The clocks runs out, time's up, 
+over. Blow” (Eminem, 2002)!
+
+After clicking enter, 
+the following 328 characters should be displayed,
+”His palms are sweaty, 
+knees weak, arms are heavy. 
+There's vomit on his sweater already, 
+mom's spaghetti. 
+He's nervous, 
+but on the surface, 
+he looks calm and ready. 
+To drop bombs, 
+but he keeps on forgettin. 
+What he wrote down, 
+the whole crowd goes so loud. 
+He opens his mouth, 
+but the words won't come out. 
+He's chokin' how, 
+everybody's jokin' now” (Eminem, 2002).
+
+due to the 328-character limit per object.
+
+	 */
 }
