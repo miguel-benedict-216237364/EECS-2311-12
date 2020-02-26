bbb34421573b6ab22693f8ce03c30e4b20d327e1
babbu16 <babbu@my.yorku.ca>
2020-02-26 16:45:07 -0500

    Changes

--- venn/src/main/java/edsVenn/Main.java
+++ venn/src/main/java/edsVenn/Main.java
@@ -82,5 +82,57 @@ everybody's jokin' now” (Eminem, 2002).
 
 due to the 328-character limit per object.
 
-	 */
+	 
+	
+	“His palms 
+	are sweaty, 
+	knees weak, 
+	arms are heavy. 
+	There's vomit on 
+	his sweater already, 
+	mom's spaghetti. He's nervous, 
+	but on the surface, 
+	he looks calm and ready. 
+	To drop bombs, but he keeps on 
+	forgettin. 
+	What he wrote down, 
+	the whole crowd goes so loud. 
+	He opens his mouth, 
+	but the words 
+	won't come out. 
+	He's chokin' how, 
+	everybody's jokin' now. 
+	The clocks runs out, 
+	time's up, 
+	over. Blow” 
+	(Eminem, 2002)!
+
+	After clicking enter, 
+	the 
+	following 328 
+	characters should 
+	be displayed,
+	”His palms are sweaty, 
+	knees weak, 
+	arms are heavy. 
+	There's vomit on 
+	his sweater already, 
+	mom's spaghetti. 
+	He's nervous, 
+	but on the surface, 
+	he looks calm 
+	and ready. 
+	To drop bombs, 
+	but he keeps on forgettin. 
+	What he wrote down, 
+	the whole 
+	crowd goes so loud. 
+	He opens his mouth, 
+	but the words won't come out. 
+	He's chokin' how, 
+	everybody's jokin' now” (Eminem, 2002).
+
+	due to the 328-character limit per object.
+
+		 */
 }
