7081a6cc8feb29bee1d03aabed4c2a0fe7a806e8
babbu16 <babbu@my.yorku.ca>
2020-03-07 17:23:13 -0500

    Design

--- venn/src/main/java/edsVenn/Main.java
+++ venn/src/main/java/edsVenn/Main.java
@@ -134,5 +134,230 @@ due to the 328-character limit per object.
 
 	due to the 328-character limit per object.
 
+“His palms 
+are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. He's nervous, 
+but on the surface, 
+he looks calm and ready. 
+To drop bombs, but he keeps on 
+forgettin. 
+What he wrote down, 
+the whole crowd goes so loud. 
+He opens his mouth, 
+but the words 
+won't come out. 
+He's chokin' how, 
+everybody's jokin' now. 
+The clocks runs out, 
+time's up, 
+over. Blow” 
+(Eminem, 2002)!
+
+After clicking enter, 
+the 
+following 328 
+characters should 
+be displayed,
+”His palms are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. 
+He's nervous, 
+but on the surface, 
+he looks calm 
+and ready. 
+To drop bombs, 
+but he keeps on forgettin. 
+What he wrote down, 
+the whole 
+crowd goes so loud. 
+He opens his mouth, 
+but the words won't come out. 
+He's chokin' how, 
+everybody's jokin' now” (Eminem, 2002).
+
+due to the 328-character limit per object.
+
+/* Acceptance Test Cases
+	 * •	If the user clicks 
+	 * the middle 
+	 * of the Venn 
+	 * diagram and types, 
+	 * after deleting 
+	 * ‘This is text number 1’:
+		 */
+	/* Acceptance Test Cases
+	 * •	If the user clicks 
+	 * the middle 
+	 * of the Venn 
+	 * diagram and types, 
+	 * after deleting 
+	 * ‘This is text number 1’:
+
+“His palms 
+are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. He's nervous, 
+but on the surface, 
+he looks calm and ready. 
+To drop bombs, but he keeps on 
+forgettin. 
+What he wrote down, 
+the whole crowd goes so loud. 
+He opens his mouth, 
+but the words 
+won't come out. 
+He's chokin' how, 
+everybody's jokin' now. 
+The clocks runs out, 
+time's up, 
+over. Blow” 
+(Eminem, 2002)!
+
+After clicking enter, 
+the 
+following 328 
+characters should 
+be displayed,
+”His palms are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. 
+He's nervous, 
+but on the surface, 
+he looks calm 
+and ready. 
+To drop bombs, 
+but he keeps on forgettin. 
+What he wrote down, 
+the whole 
+crowd goes so loud. 
+He opens his mouth, 
+but the words won't come out. 
+He's chokin' how, 
+everybody's jokin' now” (Eminem, 2002).
+
+due to the 328-character limit per object.
+
+	 
+	
+	“His palms 
+	are sweaty, 
+	knees weak, 
+	arms are heavy. 
+	There's vomit on 
+	his sweater already, 
+	mom's spaghetti. He's nervous, 
+	but on the surface, 
+	he looks calm and ready. 
+	To drop bombs, but he keeps on 
+	forgettin. 
+	What he wrote down, 
+	the whole crowd goes so loud. 
+	He opens his mouth, 
+	but the words 
+	won't come out. 
+	He's chokin' how, 
+	everybody's jokin' now. 
+	The clocks runs out, 
+	time's up, 
+	over. Blow” 
+	(Eminem, 2002)!
+
+	After clicking enter, 
+	the 
+	following 328 
+	characters should 
+	be displayed,
+	”His palms are sweaty, 
+	knees weak, 
+	arms are heavy. 
+	There's vomit on 
+	his sweater already, 
+	mom's spaghetti. 
+	He's nervous, 
+	but on the surface, 
+	he looks calm 
+	and ready. 
+	To drop bombs, 
+	but he keeps on forgettin. 
+	What he wrote down, 
+	the whole 
+	crowd goes so loud. 
+	He opens his mouth, 
+	but the words won't come out. 
+	He's chokin' how, 
+	everybody's jokin' now” (Eminem, 2002).
+
+	due to the 328-character limit per object.
+
+“His palms 
+are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. He's nervous, 
+but on the surface, 
+he looks calm and ready. 
+To drop bombs, but he keeps on 
+forgettin. 
+What he wrote down, 
+the whole crowd goes so loud. 
+He opens his mouth, 
+but the words 
+won't come out. 
+He's chokin' how, 
+everybody's jokin' now. 
+The clocks runs out, 
+time's up, 
+over. Blow” 
+(Eminem, 2002)!
+
+After clicking enter, 
+the 
+following 328 
+characters should 
+be displayed,
+”His palms are sweaty, 
+knees weak, 
+arms are heavy. 
+There's vomit on 
+his sweater already, 
+mom's spaghetti. 
+He's nervous, 
+but on the surface, 
+he looks calm 
+and ready. 
+To drop bombs, 
+but he keeps on forgettin. 
+What he wrote down, 
+the whole 
+crowd goes so loud. 
+He opens his mouth, 
+but the words won't come out. 
+He's chokin' how, 
+everybody's jokin' now” (Eminem, 2002).
+
+due to the 328-character limit per object.
+
+/* Acceptance Test Cases
+	 * •	If the user clicks 
+	 * the middle 
+	 * of the Venn 
+	 * diagram and types, 
+	 * after deleting 
+	 * ‘This is text number 1’:
 		 */
 }
