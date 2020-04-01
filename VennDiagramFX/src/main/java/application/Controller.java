32e78626bccf1528e4e2d2e5a1f09ac6401cb9c1
Drawde89 <eds89@my.yorku.ca>
2020-04-01 13:02:01 -0400

    Added Controller Copy Class to perform Deep Copy for undo/redo

--- VennDiagramFX/src/main/java/application/Controller.java
+++ VennDiagramFX/src/main/java/application/Controller.java
@@ -16,6 +16,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -55,11 +56,11 @@ public class Controller implements Initializable {
 	@FXML
 	private ComboBox<String> designTabComboBox;
 	@FXML
-	private TextField mainTitle;
+	 TextField mainTitle;
 	@FXML
-	private TextField leftTitle;
+	 TextField leftTitle;
 	@FXML
-	private TextField rightTitle;
+	 TextField rightTitle;
 	@FXML
 	private AnchorPane Window;
 	@FXML
@@ -69,9 +70,9 @@ public class Controller implements Initializable {
 	@FXML
 	private Slider sizeSlider;
 	@FXML
-	private Circle leftCircle;
+	 Circle leftCircle;
 	@FXML
-	private Circle rightCircle;
+	 Circle rightCircle;
 	@FXML
 	private Label warningLabel;
 	@FXML
@@ -167,8 +168,8 @@ public class Controller implements Initializable {
 
 	static ArrayList<Label> labelList = new ArrayList<Label>();
 
-	static ObservableList<TextField> textFieldTitle = FXCollections
-			.observableArrayList(tf -> new Observable[] { tf.textProperty() });
+	static ObservableList<TextField> textFieldTitle = FXCollections.observableArrayList(tf -> new Observable[] { tf.textProperty() });
+	static ArrayList<ControllerCopy> undoStack = new ArrayList<ControllerCopy>();
 
 	// static ObservableList<Label> labelListObservable =
 	// FXCollections.observableArrayList(labelList);
@@ -249,6 +250,8 @@ public class Controller implements Initializable {
 		});
 
 		mainTitle.textProperty().addListener(new ChangeListener<String>() {
+			
+			
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 
@@ -615,6 +618,7 @@ public class Controller implements Initializable {
 		});
 
 		centrePane.onMouseReleasedProperty().set(new EventHandler<MouseEvent>() {
+			
 			@Override
 			public void handle(MouseEvent event) {
 				if (event.getButton().equals(MouseButton.PRIMARY)) {
@@ -636,13 +640,13 @@ public class Controller implements Initializable {
 					}
 					if (isOnTop && isAddLabel.isSelected() && event.isStillSincePress()) {
 						addLabel(event);
+						undoStack.add(new ControllerCopy(labelList, leftCircle, rightCircle, leftTitle, rightTitle, mainTitle ));
 					}
 				} else {
 					removeFocusToList();
 				}
 			}
 		});
-
 		isBold.selectedProperty().addListener(new ChangeListener<Boolean>() {
 			@Override
 			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
